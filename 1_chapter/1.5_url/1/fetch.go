c03b9cfe7eee1f0a3b6ff3d5206a9b40bc4b2f7b
Kosh_book <i@example.com>
2025-11-05 07:23:53 +0300

    Упражнение 1.7 — использование io.Copy

--- 1_chapter/1.5_url/1/fetch.go
+++ 1_chapter/1.5_url/1/fetch.go
@@ -15,12 +15,11 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %\n", err)
 			os.Exit(1)
 		}
-		b, err := io.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
