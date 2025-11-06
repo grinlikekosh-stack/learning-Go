20033369f280cc2545858cc329af808cc0031353
Kosh_book <i@example.com>
2025-11-06 07:34:38 +0300

    ch_1.6. Параллельная выборка URL

---
+++ 1_chapter/1.6_Паралельная_выборка_URL/fletchall.go
@@ -0,0 +1,40 @@
+// Fetchall выполняет параллельные запросы по URL-адресам и сообщает
+// о затраченном времени и размере ответа для каждого из них.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // запуск go-подпрограммы
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // получение из канала ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // отправка в канал ch
+		return
+	}
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close() // Исключение утечки ресурсов
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
