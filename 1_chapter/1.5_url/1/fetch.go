296b010b63968c05643d5dd8fa7b222f307e1a58
Kosh_book <i@example.com>
2025-11-06 06:51:25 +0300

    1.7.8_srtings.hasprefix

--- 1_chapter/1.5_url/1/fetch.go
+++ 1_chapter/1.5_url/1/fetch.go
@@ -6,15 +6,20 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %\n", err)
 			os.Exit(1)
 		}
+
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
